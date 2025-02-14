c30941c62bdbf49c747a69a1c02e9052c3563cb8
Assil Jaby <altf4irl@gmail.com>
2025-02-14 13:55:52 +0300

    refractor: change test to run from deploy

--- script/deployFundMe.s.sol
+++ script/deployFundMe.s.sol
@@ -7,9 +7,11 @@ import {FundMe} from "../src/FundMe.sol";
 contract DeployFundMe is Script {
     address price_feed_addr = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
 
-    function run() external {
+    function run() external returns (FundMe) {
         vm.startBroadcast();
-        new FundMe(price_feed_addr);
+        FundMe fundMe = new FundMe(price_feed_addr);
         vm.stopBroadcast();
+
+        return fundMe;
     }
 }
